3fcfc0dec6659328dc79d7c5f1d54beada27bf2b
BarryByte <abhayraj.12667@gmail.com>
2025-03-06 11:24:15 +0530

    static and dynamic rendering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,13 +15,16 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
+    // now your whole page is blocked from showing UI to the visitor while the data is being fetched. Which brings us to a common challenge developers have to solve:
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+// With dynamic rendering, your application is only as fast as your slowest data fetch.
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
